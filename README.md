b2fa5d6b5c789b6aee182c475086cb8a125667ef
Felix Klein <felix@felixklein.com>
2019-12-12 18:50:00 +0100

    updates

--- README.md
+++ README.md
@@ -4,13 +4,18 @@ DNSCrypt Proxy in Docker
 ## building
 see `cli.sh`
 
+## tags
+`latest, 2.x.x, 2` -> dnscrypt-proxy 2.0, x86
+`latest-rpi, 2.x.x-rpi, 2-rpi` -> dnscrypt-proxy 2.0, arm64 (for raspberry pi)
+
 ## usage
 docker-compose example:
+
 ```yaml
 version: "2.4"
 services:
   dnscrypt-proxy:
-    image: fekle/dnscrypt-proxy:latest-rpi
+    image: fekle/dnscrypt-proxy:latest
     restart: always
     container_name: dnscrypt-proxy
     network_mode: host
@@ -22,6 +27,11 @@ services:
       nofile:
         soft: 90000
         hard: 90000
-    volumes:
-      - /home/docker/dnscrypt-proxy/data/config:/etc/dnscrypt-proxy
+## custom dnscrypt-proxy config
+#    volumes:
+#      - /home/docker/dnscrypt-proxy/data/config:/etc/dnscrypt-proxy
 ```
+
+## specifying your own dnscrypt config
+The container uses the example dnscrypt-proxy config by default, which should work fine for many applications.
+If you want to specify your own DNSCrypt config, mount the directory containing `dnscrypt-proxy.toml` in `/etc/dnscrypt-proxy`, as shown in the docker-compose example above.
--- cli.sh
+++ cli.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
+
+# ensure that we are in the correct dir
 cd "$(dirname "${0}")"
 
+# base image ref
 IMAGE_REF="fekle/dnscrypt-proxy"
 
+# dnscrypt proxy version (see https://github.com/DNSCrypt/dnscrypt-proxy/releases)
 DNSCRYPT_PROXY_VERSION=2.0.35
 DNSCRYPT_PROXY_SHORT_VERSION=2
 
+# build <arch> <tag suffix>
+# builds the docker image for the given arch, appending the tag suffix to the tag if present
 function build() {
+  DNSCRYPT_PROXY_ARCH="${1}"
+  TAG_SUFFIX="${2:-}"
   buildctl build --frontend=dockerfile.v0 --local context=. --local dockerfile=. \
     --output "type=image,\"name=${IMAGE_REF}:latest${TAG_SUFFIX},${IMAGE_REF}:${DNSCRYPT_PROXY_VERSION}${TAG_SUFFIX},${IMAGE_REF}:${DNSCRYPT_PROXY_SHORT_VERSION}${TAG_SUFFIX}\",push=true" \
     --opt "build-arg:DNSCRYPT_PROXY_ARCH=${DNSCRYPT_PROXY_ARCH}" \
     --opt "build-arg:DNSCRYPT_PROXY_VERSION=${DNSCRYPT_PROXY_VERSION}"
 }
 
+# arch check <arch>
+# ensures that corresponding images are only build on the correct architecture
 function arch_check() {
   arch="$(uname -p)"
   if [[ ${arch} != "${1}" ]]; then
@@ -22,20 +32,15 @@ function arch_check() {
   fi
 }
 
+# cli
 case "${1:-}" in
 build-x86)
-  TAG_SUFFIX=""
-  DNSCRYPT_PROXY_ARCH=linux_x86_64
-
   arch_check x86_64
-  build
+  build linux_x86_64
   ;;
 build-rpi)
-  TAG_SUFFIX="-rpi"
-  DNSCRYPT_PROXY_ARCH=linux_arm64
-
   arch_check aarch64
-  build
+  build linux_arm64 -rpi
   ;;
 build | build-all)
   ${0} build-x86
